21eb565328475b4ad13742239e5e3d3735091abd
nsg <2643198646@qq.com>
2019-09-03 10:51:51 +0800

    修改了冲突--bingqun

--- src/main.js
+++ src/main.js
@@ -8,14 +8,6 @@ import router from './router';
 Vue.config.productionTip = false
 
 
-//引入Grid 宫格组件
-import { Grid, GridItem } from 'vant';
-Vue.use(Grid).use(GridItem);
-
-//引入Tab 标签页组件
-import { Tab, Tabs } from 'vant';
-Vue.use(Tab).use(Tabs);
-
 new Vue({
   router,
   render: h => h(App),
