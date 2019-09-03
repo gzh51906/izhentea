8ec2b95e8d6cc1e15a23ee71737767851b943bb7
nsg <2643198646@qq.com>
2019-09-03 21:54:02 +0800

    222

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,6 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
-  <router-view></router-view>
-  <van-tabbar v-model="active" active-color="#000" inactive-color="#ccc" route>
-    <van-tabbar-item :icon="item.icon" v-for="item in menu" :key="item.name" :to="item.path" replace >
-    {{item.text}}
-    </van-tabbar-item>
-
-  </van-tabbar>
-=======
     <router-view></router-view>
->>>>>>> dev
   </div>
 </template>
 
